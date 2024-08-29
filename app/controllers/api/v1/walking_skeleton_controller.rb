8f7bb59d69a9b65ba29119aef6ce730f55bc1bbe
River Ke <river.ke@lig.com.tw>
2024-08-29 10:56:44 +0800

    🎨 show time at rails's timezone

--- app/controllers/api/v1/walking_skeleton_controller.rb
+++ app/controllers/api/v1/walking_skeleton_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::WalkingSkeletonController < ApplicationController
   def show
     respond_to do |format|
       format.json { render json: { message: "Walk" } }
-      format.html { render plain: "Server is up and running -- #{Time.now}" }
+      format.html { render plain: "Server is up and running -- #{Time.current}" }
     end
   end
 end
